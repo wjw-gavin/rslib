1bb532ebc31995c5260e2e795d0d99bf118a013f
SoonIter <sooniter@gmail.com>
2024-12-25 00:44:29 +0800

    chore: update

--- examples/react-component-bundle-false/src/components/CounterButton/index.tsx
+++ examples/react-component-bundle-false/src/components/CounterButton/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import logo from '../../assets/logo.svg';
 import styles from './index.module.scss';
-import logo from '../../assets/logo.svg'
 
 interface CounterButtonProps {
   onClick: () => void;
@@ -12,7 +12,7 @@ export const CounterButton: React.FC<CounterButtonProps> = ({
   label,
 }) => (
   <button type="button" className={styles.button} onClick={onClick}>
-    <img src={logo} alt='react'/>
+    <img src={logo} alt="react" />
     {label}
   </button>
 );
